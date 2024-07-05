34e83f1557b09aed44c2b81567e9cb5f1f6a2f7d
Benardo07 <117990908+Benardo07@users.noreply.github.com>
2024-07-06 03:36:55 +0700

    feat: enable cors

--- src/app/api/trpc/[trpc]/route.ts
+++ src/app/api/trpc/[trpc]/route.ts
@@ -1,34 +1,52 @@
-import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
-import { type NextRequest } from "next/server";
+import { NextResponse } from 'next/server';
+import { type NextRequest } from 'next/server';
+import { fetchRequestHandler } from '@trpc/server/adapters/fetch';
 
-import { env } from "~/env";
-import { appRouter } from "~/server/api/root";
-import { createTRPCContext } from "~/server/api/trpc";
+import { env } from '~/env';
+import { appRouter } from '~/server/api/root';
+import { createTRPCContext } from '~/server/api/trpc';
 
-/**
- * This wraps the `createTRPCContext` helper and provides the required context for the tRPC API when
- * handling a HTTP request (e.g. when you make requests from Client Components).
- */
 const createContext = async (req: NextRequest) => {
   return createTRPCContext({
     headers: req.headers,
   });
 };
 
-const handler = (req: NextRequest) =>
-  fetchRequestHandler({
-    endpoint: "/api/trpc",
+function handler(req: NextRequest) {
+  if (req.method === 'OPTIONS') {
+    const res = new Response(null, {
+      status: 204,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+      },
+    });
+    return res;
+  }
+
+  return fetchRequestHandler({
+    endpoint: '/api/trpc',
     req,
     router: appRouter,
     createContext: () => createContext(req),
-    onError:
-      env.NODE_ENV === "development"
-        ? ({ path, error }) => {
-            console.error(
-              `❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`
-            );
-          }
-        : undefined,
-  });
+    responseMeta: ({ ctx, type, errors }) => {
+      const headers = new Headers();
+      headers.append('Access-Control-Allow-Origin', '*');
 
-export { handler as GET, handler as POST };
+      // Depending on the operation and the presence of errors, you may adjust status codes
+      const status = errors ? 400 : 200;
+
+      return {
+        headers,
+        status,
+      };
+    },
+    onError: env.NODE_ENV === 'development' ? ({ path, error }) => {
+      console.error(`❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`);
+    } : undefined,
+  });
+}
+
+export { handler as GET, handler as POST, handler as OPTIONS };
+
